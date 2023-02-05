89b38cbe11db4ef2cc20cec4f1dd2d3038918d4c
Richard Lane <richard.lane@bristol.ac.uk>
2023-02-05 17:38:35 +0000

    streamlit app

--- code/main.py
+++ code/main.py
@@ -2,8 +2,9 @@
 Main driver code for whatever I decide to do
 
 """
-from pprint import pprint
+import streamlit as st
 import matplotlib.pyplot as plt
+from pprint import pformat
 
 # My stuff
 import read
@@ -21,17 +22,31 @@ def main():
     dump = read.read_dump(path)
 
     counts = read.count_messages(dump)
-    plot.plot_counts(counts, dump["title"])
+    count_plot,_ = plot.plot_counts(counts, dump["title"])
 
-    plot.plot_reacts(read.count_reactions(dump))
+    react_plot, _ = plot.plot_reacts(read.count_reactions(dump))
 
-    plot.plot_reacts(read.count_reactions(dump), total_counts=counts)
+    scaled_reacts, _ = plot.plot_reacts(read.count_reactions(dump), total_counts=counts)
 
     # Get a dict of dicts of who reacted to who
     reacts = read.count_reactions(dump, return_strs=True)
-    pprint(reacts)
 
-    plt.show()
+    # Streamlit
+    st.title("The Shakira Respecters")
+    st.write("Who talks the most:")
+    st.write(count_plot)
+
+    st.write("Who reacts to who:")
+    st.write(react_plot)
+    st.write("i am the king of reacts")
+
+    st.write("Scaled by overall counts:")
+    st.write(scaled_reacts)
+    st.write("turns out its really holly")
+
+    st.write("what reactions does everyone use")
+    st.write("I got bored here so it isnt nicely formatted")
+    st.write(pformat(reacts))
 
 
 if __name__ == "__main__":
